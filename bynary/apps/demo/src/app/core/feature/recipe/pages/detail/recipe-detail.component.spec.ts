d519b1cc4114819df83f227bc6f88a24ec1f2045
Lisa Hampel <l.hampel@bynary.io>
2021-11-09 15:29:40 +0100

    bynary(apps): edit recipe-detail.component.spec.ts after PR

--- bynary/apps/demo/src/app/core/feature/recipe/pages/detail/recipe-detail.component.spec.ts
+++ bynary/apps/demo/src/app/core/feature/recipe/pages/detail/recipe-detail.component.spec.ts
@@ -1,8 +1,11 @@
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgxsModule } from '@ngxs/store';
+import { of } from 'rxjs';
+import { MaybeMocked, mocked } from 'ts-jest/dist/utils/testing';
 import { RecipesFacade } from '../../services/recipes.facade';
 import { RecipesState } from '../../state/recipes.state';
 
@@ -11,23 +14,14 @@ import { RecipeDetailComponent } from './recipe-detail.component';
 describe('RecipeDetailComponent', () => {
     let component: RecipeDetailComponent;
     let fixture: ComponentFixture<RecipeDetailComponent>;
+    let mockedActivatedRoute: MaybeMocked<ActivatedRoute>;
+    let mockedRouter: MaybeMocked<Router>;
+    let mockedRecipeFace: MaybeMocked<RecipesFacade>;
+
     const recipe = {
         name: 'Schnitzel mit Pommes', description: 'Bla bla',
         imagePath: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Recipe_icon.svg',
         ingredients: [{ name: 'Schnitzel', amount: 2, }, { name: 'Pommes', amount: 1 }]
-    };
-    const router = {
-        navigate: jest.fn(),
-    };
-    const route = {
-        params: {
-            subscribe: jest.fn()
-        }
-    };
-    const recipesFacadeStub = {
-        getRecipe: jest.fn(),
-        addIngredientsToShoppingList: jest.fn(),
-        deleteRecipe: jest.fn()
     };
 
     beforeEach(async () => {
@@ -40,7 +34,23 @@ describe('RecipeDetailComponent', () => {
             providers: [
                 {
                     provide: RecipesFacade,
-                    useValue: recipesFacadeStub
+                    useValue: {
+                        getRecipe: jest.fn(),
+                        addIngredientsToShoppingList: jest.fn(),
+                        deleteRecipe: jest.fn()
+                    }
+                },
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        params: of({ id: 1 })
+                    }
+                },
+                {
+                    provide: Router,
+                    useValue: {
+                        navigate: jest.fn()
+                    }
                 }
             ]
         })
@@ -50,6 +60,11 @@ describe('RecipeDetailComponent', () => {
     beforeEach(async () => {
         fixture = TestBed.createComponent(RecipeDetailComponent);
         component = fixture.componentInstance;
+
+        mockedActivatedRoute = mocked(TestBed.inject(ActivatedRoute));
+        mockedRouter = mocked(TestBed.inject(Router));
+        mockedRecipeFace = mocked(TestBed.inject(RecipesFacade));
+
         component.recipe = recipe;
 
         fixture.detectChanges();
@@ -61,38 +76,40 @@ describe('RecipeDetailComponent', () => {
 
     describe('logic', () => {
 
-
-        it('should call getRecipe method', () => {
-            const routeParams = jest.spyOn(route.params, 'subscribe');
-
-            route.params.subscribe(() => {
-                recipesFacadeStub.getRecipe();
-                expect(recipesFacadeStub.getRecipe).toHaveBeenCalledTimes(1);
-            });
-
-            expect(routeParams).toHaveBeenCalledTimes(1);
+        it('should call getRecipe method when ngOnInit is called', () => {
+            expect(mockedRecipeFace.getRecipe).toHaveBeenCalledWith(1);
         });
 
         it('should call addIngredientsToShoppingList in onAddToShoppingList method', () => {
-            recipesFacadeStub.addIngredientsToShoppingList();
-            expect(recipesFacadeStub.addIngredientsToShoppingList).toHaveBeenCalledTimes(1);
+            component.onAddToShoppingList();
+            expect(mockedRecipeFace.addIngredientsToShoppingList).toHaveBeenCalledWith([
+                { name: 'Schnitzel', amount: 2, },
+                { name: 'Pommes', amount: 1 }
+            ]);
         });
 
-        it('should navigate to edit-page when call onEditRecipe method', () => {
-            router.navigate(['edit']);
-            expect(router.navigate).toHaveBeenCalledWith(['edit']);
+        it('should navigate to edit page when call onEditRecipe method', () => {
+            component.onEditRecipe();
+            expect(mockedRouter.navigate).toHaveBeenCalledWith(['edit'], { relativeTo: { params: mockedActivatedRoute.params } });
         });
 
-        it('should call deleteRecipe method and navigate to recipes page when call onDeleteRecipe method', () => {
-            recipesFacadeStub.deleteRecipe();
-            expect(recipesFacadeStub.deleteRecipe).toHaveBeenCalledTimes(1);
+        describe('onDeleteRecipe', () => {
 
-            router.navigate(['recipes']);
-            expect(router.navigate).toHaveBeenCalledWith(['recipes']);
+            beforeEach(() => {
+                component.onDeleteRecipe();
+            });
+
+            it('should call recipeFacade.deleteRecipe with the correct arguments', () => {
+                expect(mockedRecipeFace.deleteRecipe).toHaveBeenCalledWith(1);
+            });
+
+            it('should call router.navigate with the correct arguments', () => {
+                expect(mockedRouter.navigate).toHaveBeenCalledWith(['recipes']);
+            });
+
         });
 
     });
-
 
     describe('template', () => {
         let debugElement: DebugElement;
@@ -101,25 +118,42 @@ describe('RecipeDetailComponent', () => {
             debugElement = fixture.debugElement;
         });
 
-        it('should display recipe with name, image, description and ingredients', () => {
-            const recipeImage = debugElement.query(By.css('.recipe-image')).properties;
-            const recipeName = debugElement.query(By.css('.recipe-name'));
-            const recipeDescribtion = debugElement.query(By.css('.recipe-description'));
-            const recipeIngredients = debugElement.queryAll(By.css('.recipe-ingredients'));
+        describe('recipe', () => {
 
-            expect(recipeImage.src).toEqual('https://upload.wikimedia.org/wikipedia/commons/b/b9/Recipe_icon.svg');
-            expect(recipeName.nativeElement.textContent).toEqual('Schnitzel mit Pommes');
-            expect(recipeDescribtion.nativeElement.textContent).toEqual(' Bla bla ');
+            it('should have a name', () => {
+                const recipeName = debugElement.query(By.css('.c-recipe-detail-component__recipe-name'));
 
-            expect(recipeIngredients).toHaveLength(2);
-            expect(recipeIngredients[0].nativeElement.textContent).toEqual(' Schnitzel - 2 ');
-            expect(recipeIngredients[1].nativeElement.textContent).toEqual(' Pommes - 1 ');
+                expect(recipeName.nativeElement.textContent).toEqual('Schnitzel mit Pommes');
+            });
+
+            it('should have an image', () => {
+                const recipeImage = debugElement.query(By.css('.c-recipe-detail-component__recipe-image')).properties;
+
+                expect(recipeImage.src).toEqual('https://upload.wikimedia.org/wikipedia/commons/b/b9/Recipe_icon.svg');
+            });
+
+            it('should have a description', () => {
+                const recipeDescription = debugElement.query(By.css('.c-recipe-detail-component__recipe-description'));
+
+                expect(recipeDescription.nativeElement.textContent).toEqual(' Bla bla ');
+            });
+
+            it('should have a ingredients', () => {
+                const recipeIngredients = debugElement.queryAll(By.css('.c-recipe-detail-component__recipe-ingredients'));
+
+                expect(recipeIngredients).toHaveLength(2);
+                expect(recipeIngredients[0].nativeElement.textContent).toEqual(' Schnitzel - 2 ');
+                expect(recipeIngredients[1].nativeElement.textContent).toEqual(' Pommes - 1 ');
+            });
+
         });
+
+        // TODO: Überprüfen, ob Buttons richtige Texte anzeigen
 
         it('should call onAddToShoppingList method when button "To Shopping List" is clicked', () => {
             const addToShoppingList = jest.spyOn(component, 'onAddToShoppingList');
-
             const toShoppingListButton = debugElement.query(By.css('#toShoppingList'));
+
             toShoppingListButton.triggerEventHandler('click', addToShoppingList);
 
             expect(addToShoppingList).toHaveBeenCalledTimes(1);
@@ -127,8 +161,8 @@ describe('RecipeDetailComponent', () => {
 
         it('should call onEditRecipe method when button "Edit Recipe" is clicked', () => {
             const onEditRecipe = jest.spyOn(component, 'onEditRecipe');
-
             const editRecipeButton = debugElement.query(By.css('#editRecipe'));
+
             editRecipeButton.triggerEventHandler('click', onEditRecipe);
 
             expect(onEditRecipe).toHaveBeenCalledTimes(1);
@@ -136,12 +170,13 @@ describe('RecipeDetailComponent', () => {
 
         it('should call onDeleteRecipe method when button "onDeleteRecipe" is clicked', () => {
             const onDeleteRecipe = jest.spyOn(component, 'onDeleteRecipe');
-
             const deleteRecipe = debugElement.query(By.css('#deleteRecipe'));
+
             deleteRecipe.triggerEventHandler('click', onDeleteRecipe);
 
             expect(onDeleteRecipe).toHaveBeenCalledTimes(1);
         });
+
     });
 
 });
